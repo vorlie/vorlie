953cbba6704ed58977b64169330c9e3a89bfe7fc
vorlie <vorlie0961@gmail.com>
2024-09-17 17:27:22 +0200

    dd

--- src/components/react/Project.tsx
+++ src/components/react/Project.tsx
@@ -12,15 +12,18 @@ const Project: React.FC<ProjectProps> = ({ title, desc, links }) => (
             <p className="project-title">{title}</p>
             <p className="project-desc">{desc}</p>
         </div>
-        <div className="project-links">
-            {links.map((link, index) => (
-                <React.Fragment key={index}>
-                    <a className="link" href={link.href} target="_blank" rel="noopener noreferrer">
-                        {link.text}
-                    </a>
-                    {index < links.length - 1 && <span className="separator"> • </span>}
-                </React.Fragment>
-            ))}
+        <div className="project-details">
+            <hr className="project-hr" />
+            <div className="project-links">
+                {links.map((link, index) => (
+                    <React.Fragment key={index}>
+                        <a className="link" href={link.href} target="_blank" rel="noopener noreferrer">
+                            {link.text}
+                        </a>
+                        {index < links.length - 1 && <span className="separator"> • </span>}
+                    </React.Fragment>
+                ))}
+            </div>
         </div>
     </div>
 );
