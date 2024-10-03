13ff390653766326164ef9bcc78b3bab954437eb
vorlie <vorlie0961@gmail.com>
2024-10-03 02:46:30 +0200

    change images to png ones

--- src/components/react/PCSpecification.tsx
+++ src/components/react/PCSpecification.tsx
@@ -6,7 +6,7 @@ export const PCSpecification: React.FC = () => (
         <h1 className="header">My pc specification</h1>
         <div className="container">
             <Item
-                imgSrc="https://m.media-amazon.com/images/I/81b75EQJrgL._AC_SL1500_.jpg"
+                imgSrc="https://cdn.mos.cms.futurecdn.net/BekcfeBawwJ5mfZdV7ShMg.png"
                 imgAlt="cpu"
                 title="CPU"
                 desc="AMD Ryzen 5 3600"
@@ -14,7 +14,7 @@ export const PCSpecification: React.FC = () => (
                 linkText="Buy on Amazon"
             />
             <Item
-                imgSrc="https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2020/4/pr_2020_4_29_14_30_1_600_11.jpg"
+                imgSrc="https://us-east-1.tixte.net/uploads/cx.tixte.co/spc272-spc-spartan-4-max-01b-removebg-preview.png"
                 imgAlt="cpu cooler"
                 title="CPU Cooler"
                 desc="SilentiumPC Spartan 4 MAX"
@@ -22,28 +22,28 @@ export const PCSpecification: React.FC = () => (
                 linkText="See on x-kom"
             />
             <Item
-                imgSrc="https://m.media-amazon.com/images/S/aplus-media/vc/baf493e7-aaf1-4a52-88f1-8ccb8344545a.__CR0,0,300,400_PT0_SX300_V1___.jpg"
+                imgSrc="https://dlcdnimgs.asus.com/websites/global/products/afwaj0qnsbp72phf/intro-main.png"
                 imgAlt="motherboard"
                 title="Motherboard"
                 desc="ASUS Prime B450M-A II"
                 link="https://www.amazon.com/dp/B08KH12V25"
                 linkText="Buy on Amazon"/>
             <Item 
-                imgSrc="https://m.media-amazon.com/images/I/81yBmcZTERL._AC_SL1500_.jpg"
+                imgSrc="https://dlcdnwebimgs.asus.com/gain/0747863d-64ac-4ed8-b647-7a574d90d5a9/"
                 imgAlt="gpu"
                 title="GPU"
                 desc="ASUS DUAL OC V2 Radeon RX 7600 8GB"
                 link="https://www.amazon.com/dp/B0C7JP2SQ2"
                 linkText="Buy on Amazon"/>
             <Item 
-                imgSrc="https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2017/7/pr_2017_7_21_9_20_19_508.jpg"
+                imgSrc="https://www.safepcdirect.co.uk/media/catalog/product/cache/1/thumbnail/180x240/9df78eab33525d08d6e5fb8d27136e95/4/_/4_13.png"
                 imgAlt="ram"
                 title="RAM"
                 desc="Corsair Vengeance LPX 16 GB (2 x 8 GB) DDR4-3200 CL16"
                 link="https://www.amazon.com/dp/B0143UM4TC"
                 linkText="Buy on Amazon"/>
             <Item
-                imgSrc="https://cdn.x-kom.pl/i/setup/images/prod/big/product-new-big,,2022/10/pr_2022_10_3_8_58_52_937_00.jpg"
+                imgSrc="https://us-east-1.tixte.net/uploads/cx.tixte.co/px500-1-1920x960-removebg-preview.png"
                 imgAlt="nvme"
                 title="Storage: NVMe"
                 desc="Goodram PX500 PCIe GEN 3 x4 NVMe 256GB"
@@ -51,7 +51,7 @@ export const PCSpecification: React.FC = () => (
                 linkText="Buy on x-kom"
             />
             <Item
-                imgSrc="https://m.media-amazon.com/images/I/61yXRHMJTDL._AC_SL1500_.jpg"
+                imgSrc="https://us-east-1.tixte.net/uploads/cx.tixte.co/71xUljwiMTL-removebg-preview.png"
                 imgAlt="hdd"
                 title="Storage: HDD"
                 desc="TOSHIBA MQ01ABD100 1TB 2,5"
@@ -59,7 +59,7 @@ export const PCSpecification: React.FC = () => (
                 linkText="Buy on Amazon"
             />
             <Item
-                imgSrc="https://m.media-amazon.com/images/I/81WsV5yKboL._AC_SL1500_.jpg"
+                imgSrc="https://a.storyblok.com/f/281110/2a55d7361e/1-500w-mwe-white-230v-v2.png/m/960x0/smart"
                 imgAlt="power supply"
                 title="Power Supply"
                 desc="Cooler Master MWE V2 500W 80 Plus"
@@ -67,7 +67,7 @@ export const PCSpecification: React.FC = () => (
                 linkText="Buy on Amazon"
             />
             <Item
-                imgSrc="https://m.media-amazon.com/images/I/81I9Ef0fOIL._AC_SL1500_.jpg"
+                imgSrc="https://a.storyblok.com/f/281110/5c1192c34c/gallery-2-min.png/m/960x0/smart"
                 imgAlt="pc case"
                 title="PC Case"
                 desc="Cooler Master MasterBox Q300L"
