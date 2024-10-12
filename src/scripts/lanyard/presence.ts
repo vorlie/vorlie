226bdfc63475dbc7594e2b5ca59efafe7f4cf1d9
vorlie <vorlie0961@gmail.com>
2024-10-12 20:34:48 +0200

    d

--- src/scripts/lanyard/presence.ts
+++ src/scripts/lanyard/presence.ts
@@ -60,7 +60,7 @@ export const displayPresence = async (presence: LanyardData | null): Promise<voi
         let endTimestamp = activity.timestamps?.end;
         let imageUrl = '/images/default.png'; 
         if (activity.name.toLowerCase().includes('spotify') && presence.spotify) {
-            let truncatedName = truncateText(`Listening to ${presence.spotify.song}`, 28);
+            let truncatedName = truncateText(`${presence.spotify.song}`, 28);
             let truncatedDetails = truncateText(`by ${presence.spotify.artist}`, 25);
             let truncatedState = truncateText(presence.spotify.song, 25);
             activityName = truncatedName;
