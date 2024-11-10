f72f5cb0616bdd72fa15298664e99cec8f22f61c
vorlie <vorlie0961@gmail.com>
2024-11-10 11:48:32 +0100

    Make the extract image url better

--- src/scripts/lanyard/utils.ts
+++ src/scripts/lanyard/utils.ts
@@ -1,13 +1,20 @@
 // utils.ts
 export const extractImageUrl = (url: string, application_id?: string): string => {
-    if (url.startsWith('mp:external/')) {
-        return `https://media.discordapp.net/external/${url.replace('mp:external/', '')}`;
-    } else if (url.startsWith("spotify:")) {
-        return url.replace("spotify:", "https://i.scdn.co/image/");
-    } else if (application_id) {
-        return `https://cdn.discordapp.com/app-assets/${application_id}/${url}.webp`;
+    if (!url) return '/images/default.png'; // Fallback if url is empty
+
+    try {
+        if (url.startsWith('mp:external/')) {
+            return `https://media.discordapp.net/external/${url.replace('mp:external/', '')}`;
+        } else if (url.startsWith("spotify:")) {
+            return url.replace("spotify:", "https://i.scdn.co/image/");
+        } else if (application_id) {
+            return `https://cdn.discordapp.com/app-assets/${application_id}/${url}.png`;
+        }
+        return url;
+    } catch (error) {
+        console.error("Error extracting image URL:", error);
+        return '/images/default.png'; // Return placeholder if an error occurs
     }
-    return url;
 };
 
 export const formatElapsedTime = (start: number, end?: number): string => {
