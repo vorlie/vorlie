ed8759a886676f4a292034ca74d3080ccf625162
vorlie <vorlie0961@gmail.com>
2025-03-06 15:17:31 +0100

    feat: add invite link for Miko bot to redirect script

--- src/scripts/redirect.ts
+++ src/scripts/redirect.ts
@@ -6,6 +6,7 @@ export default function redirect() {
 
         const keywordMap: { [key: string]: string } = {
             discord: 'https://discord.gg/yUueAFyAmN',
+            invite_miko: 'https://discord.com/oauth2/authorize?client_id=1060061912710258699&permissions=10036764339318&integration_type=0&scope=applications.commands+bot',
             miko_support: 'https://discord.gg/yUueAFyAmN',
             youtube: 'https://www.youtube.com/@vve1_',
             github: 'https://github.com/vorlie',
