0a9bfe6915ec9a4e81561536de7cb0cc3467d485
vorlie <vorlie0961@gmail.com>
2025-11-18 23:43:05 +0100

    update snake description

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,7 +8,7 @@ import { SiElectron, SiJavascript, SiCplusplus, SiRust } from "react-icons/si";
 const projectData = [
   {
     title: "Snake Shader",
-    desc: " Snake Shader is a modern take on the classic Snake game, built using Pygame for input/window handling and ModernGL for GPU-accelerated graphics.",
+    desc: "Welcome to Snake Shader, a high-performance, retro-futuristic reimplementation of the classic Snake game. We've ditched the basic 2D grids for a fully GPU-accelerated experience using ModernGL and Pygame",
     links: [
       {
         href: "https://github.com/vorlie/snake-shader",
