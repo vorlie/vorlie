a89f71ecfdea3eeff5a369bb9f4430316dd54209
vorlie <vorlie0961@gmail.com>
2026-04-21 06:53:11 +0200

    Use fetch instead of ASSETS.fetch for external API

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -113,7 +113,7 @@ export const onRequest = async (context: any) => {
     if (clipId) {
       try {
         const clipsReq = new Request(`https://api.vorlie.pl/clips.json`);
-        const clipsRes = await context.env.ASSETS.fetch(clipsReq);
+        const clipsRes = await fetch(clipsReq);
         if (clipsRes.ok) {
           const clips: any[] = await clipsRes.json();
           const clip = clips.find((c) => c.id === clipId);
