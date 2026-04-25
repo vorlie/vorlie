725fe24c53276872ce4d4cbb508506c89d54a60c
vorlie <vorlie0961@gmail.com>
2026-04-25 17:46:54 +0200

    update repo url

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -19,7 +19,10 @@ const projectData = [
     desc: "A browser-based puzzle game teaching JavaScript fundamentals through real-time code execution and system-repair scenarios. Built with React & Tailwind CSS.",
     links: [
       { href: "https://secure-terminal.vorlie.pl/", text: "Homepage" },
-      { href: "https://github.com/vorlie/secure-terminal", text: "Repository" },
+      {
+        href: "https://github.com/vorlie/SECURE_TERMINAL_V1.9",
+        text: "Repository",
+      },
     ],
     languages: ["React", "Typescript"],
   },
